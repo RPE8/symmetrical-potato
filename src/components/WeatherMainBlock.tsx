b17f1cab0289f45d346ef15481f410c1d885be21
RPE8 <vitali.shuman96@gmail.com>
2023-03-28 14:52:50 +0300

    Add some shareable styles of WeatherMainBlock to cva varinats

--- src/components/WeatherMainBlock.tsx
+++ src/components/WeatherMainBlock.tsx
@@ -1,5 +1,40 @@
 import WeatherIcon from "./ui/WeatherIcon";
 import type { Weather } from "@/utils/constants";
+import { cva, VariantProps } from "class-variance-authority";
+import { twMerge } from "tailwind-merge";
+
+const weatherIconVariants = cva("", {
+  variants: {
+    loading: {
+      true: "h-32 w-32 animate-pulse bg-gray-c2 rounded-full",
+    },
+  },
+  defaultVariants: {
+    loading: false,
+  },
+});
+
+const locationVariants = cva("text-3xl font-bold my-5", {
+  variants: {
+    loading: {
+      true: "animate-pulse bg-gray-c2 rounded-md w-32",
+    },
+  },
+  defaultVariants: {
+    loading: false,
+  },
+});
+
+const temperatureVariants = cva("text-5xl fond-semibold", {
+  variants: {
+    loading: {
+      true: "animate-pulse bg-gray-c2 rounded-md w-32",
+    },
+  },
+  defaultVariants: {
+    loading: false,
+  },
+});
 
 interface WeatherMainBlockProps {
   weather: Weather | undefined;
@@ -20,11 +55,11 @@ const WeatherMainBlock = ({
   if (isLoading) {
     return (
       <div className="flex flex-col items-center my-10">
-        <div className="w-32 h-32 rounded-full bg-gray-c2 animate-pulse" />
-        <div className="text-3xl w-32 font-bold my-5 animate-pulse bg-gray-c2 rounded-md">
+        <div className={twMerge(weatherIconVariants({ loading: isLoading }))} />
+        <div className={twMerge(locationVariants({ loading: isLoading }))}>
           &nbsp;
         </div>
-        <div className="text-5xl w-32 animate-pulse bg-gray-c2 rounded-md">
+        <div className={twMerge(temperatureVariants({ loading: isLoading }))}>
           &nbsp;
         </div>
       </div>
@@ -35,10 +70,15 @@ const WeatherMainBlock = ({
   return (
     <>
       <div className="flex flex-col items-center my-10">
-        {weather && <WeatherIcon weather={weather} />}
-        <h1 className="text-3xl font-bold my-5">{location}</h1>
+        {weather && (
+          <WeatherIcon
+            weather={weather}
+            className={twMerge(weatherIconVariants({}))}
+          />
+        )}
+        <h1 className={twMerge(locationVariants({}))}>{location}</h1>
 
-        <h2 className="text-5xl font-semibold">
+        <h2 className={twMerge(temperatureVariants({}))}>
           {temperature} &deg;{temperatureUnit}
         </h2>
       </div>
