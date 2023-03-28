d3b8a25bf5b23fd68afbd3ecb833cab59d753fde
RPE8 <vitali.shuman96@gmail.com>
2023-03-28 14:43:35 +0300

    Add isLoading prop handler for WeatherMainBlock

--- src/components/WeatherMainBlock.tsx
+++ src/components/WeatherMainBlock.tsx
@@ -1,29 +1,62 @@
 import WeatherIcon from "./ui/WeatherIcon";
 import type { Weather } from "@/utils/constants";
+import { cva, VariantProps } from "class-variance-authority";
+import { twMerge } from "tailwind-merge";
 
-interface WeatherMainBlockProps {
-  weather: Weather;
-  temperature: number;
-  temperatureUnit: string;
-  weatherDescription: string;
-  location: string;
+const WeatherMainBlockVariants = cva("", {
+  variants: {
+    loading: {
+      true: "animate-pulse bg-gray-c2 rounded-md",
+    },
+  },
+  defaultVariants: {
+    loading: false,
+  },
+});
+
+interface WeatherMainBlockProps
+  extends VariantProps<typeof WeatherMainBlockVariants> {
+  weather: Weather | undefined;
+  temperature: number | undefined;
+  temperatureUnit: string | undefined;
+  weatherDescription: string | undefined;
+  location: string | undefined;
+  isLoading?: boolean;
 }
 
 const WeatherMainBlock = ({
   weather,
   temperatureUnit,
   temperature,
+  isLoading = false,
   location,
 }: WeatherMainBlockProps) => {
-  return (
-    <div className="flex flex-col items-center my-10">
-      {weather && <WeatherIcon weather={weather} />}
-      <h1 className="text-3xl font-bold my-5">{location}</h1>
+  if (isLoading) {
+    return (
+      <div className="flex flex-col items-center my-10">
+        <div className="w-32 h-32 rounded-full bg-gray-c2 animate-pulse" />
+        <div className="text-3xl w-32 font-bold my-5 animate-pulse bg-gray-c2 rounded-md">
+          &nbsp;
+        </div>
+        <div className="text-5xl w-32 animate-pulse bg-gray-c2 rounded-md">
+          &nbsp;
+        </div>
+      </div>
+    );
+  }
+  if (!location || temperature == null) return null;
 
-      <h2 className="text-5xl font-semibold">
-        {temperature} &deg;{temperatureUnit}
-      </h2>
-    </div>
+  return (
+    <>
+      <div className="flex flex-col items-center my-10">
+        {weather && <WeatherIcon weather={weather} />}
+        <h1 className="text-3xl font-bold my-5">{location}</h1>
+
+        <h2 className="text-5xl font-semibold">
+          {temperature} &deg;{temperatureUnit}
+        </h2>
+      </div>
+    </>
   );
 };
 
