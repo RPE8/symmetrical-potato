4830dc7bc4d29bb6583712c4920ecfdc8f958303
RPE8 <vitali.shuman96@gmail.com>
2023-03-31 15:55:49 +0300

    Add Sunrise, Sunset and Horizin related labels to chart

--- src/components/SunsetSunriseChart.tsx
+++ src/components/SunsetSunriseChart.tsx
@@ -11,6 +11,7 @@ interface SunsetSunriseChartProps {
   currentTime: number;
 }
 
+const margin = { top: 40, right: 0, bottom: 0, left: 0 };
 const xType = d3.scaleTime;
 const yType = d3.scaleLinear;
 
@@ -21,6 +22,33 @@ const SunsetSunriseChart = ({
 }: SunsetSunriseChartProps) => {
   const [size, setSize] = useState([0, 0]);
   const svgRef = useRef<SVGSVGElement>(null);
+
+  const placeSunsetSunrise = useCallback(
+    ({
+      svg,
+      xScale,
+      yScale,
+      timeSelector,
+      textSelector,
+      groupSelector,
+      date,
+      highest,
+    }) => {
+      const sunriseTime = svg.select(timeSelector);
+      const sunriseTimeBounds = sunriseTime.node().getBBox();
+
+      sunriseTime
+        .attr("x", xScale(date) - sunriseTimeBounds.width / 2)
+        .attr("y", yScale(highest));
+      const sunriseText = svg.select(textSelector);
+      const sunriseTextBounds = sunriseText.node().getBBox();
+      sunriseText
+        .attr("x", xScale(date) - sunriseTextBounds.width / 2)
+        .attr("y", yScale(highest) - sunriseTextBounds.height);
+      svg.select(groupSelector).attr("transform", "translate(0, -10)");
+    },
+    []
+  );
 
   const handleResize = useCallback(() => {
     if (svgRef.current) {
@@ -38,12 +66,14 @@ const SunsetSunriseChart = ({
       handleResize();
       return;
     }
+
     window.addEventListener("resize", handleResize);
+
     const width = size[0];
     const height = size[1];
 
     const xRange = [0, width];
-    const yRange = [height, 0];
+    const yRange = [height, margin.top];
 
     const startDate = startOfDay(currentTime);
     const endDate = endOfDay(currentTime);
@@ -140,44 +170,72 @@ const SunsetSunriseChart = ({
 
     svg.select("#startToSunrise").datum(startToSunriseData).attr("d", area);
     svg.select("#sunriseToSunset").datum(sunriseToSunsetData).attr("d", area);
-
     svg.select("#horizontalLine").datum(horizontalLineData).attr("d", line);
-
     svg.select("#sunriseLine").datum(sunriseLineData).attr("d", line);
-
     svg.select("#sunsetLine").datum(sunsetLineData).attr("d", line);
 
-    const sunriseText = svg.select("#sunrise");
-    const sunriseTextBounds = sunriseText.node().getBBox();
+    // Placenment of sunrise time and text
+    placeSunsetSunrise({
+      svg,
+      xScale,
+      yScale,
+      timeSelector: "#sunriseTime",
+      textSelector: "#sunrise",
+      groupSelector: "#sunrise-g",
+      date: sunriseDate,
+      highest: sunriseHighest,
+    });
 
-    sunriseText
-      .attr("x", xScale(sunriseDate) - sunriseTextBounds.width / 2)
-      .attr("y", yScale(sunriseHighest));
+    // Placenment of sunset time and text
+    placeSunsetSunrise({
+      svg,
+      xScale,
+      yScale,
+      timeSelector: "#sunsetTime",
+      textSelector: "#sunset",
+      groupSelector: "#sunset-g",
+      date: sunsetDate,
+      highest: sunsetHighest,
+    });
 
-    const sunsetText = svg.select("#sunset");
-    const sunsetTextBounds = sunsetText.node().getBBox();
-
-    sunsetText
-      .attr("x", xScale(sunsetDate) - sunsetTextBounds.width / 2)
-      .attr("y", yScale(sunsetHighest));
+    const horizon = svg.select("#horizon");
+    const horizonBounds = horizon.node().getBBox();
+    horizon.attr(
+      "transform",
+      `translate(${width - horizonBounds.width}, ${yScale(0) - 10})`
+    );
 
     return () => {
       window.removeEventListener("resize", handleResize);
     };
-  }, [sunset, sunrise, currentTime, handleResize, size]);
+  }, [sunset, sunrise, currentTime, handleResize, size, placeSunsetSunrise]);
 
   return (
     <svg
       ref={svgRef}
       width="100%"
       height="100%"
-      className="aspect-w-16 aspect-h-9"
       viewBox={[0, 0, size[0], size[1]]}
     >
-      <text id="sunriseTime">{format(currentTime, "hh:mm a")}</text>
-      <text id="sunrise">{format(currentTime, "hh:mm a")}</text>
-      <text id="sunrise">Sunrise</text>
-      <text id="sunset">Sunset</text>
+      <g id="sunrise-g">
+        <text id="sunrise" className="text-sm fill-gray-c2">
+          Sunrise
+        </text>
+        <text id="sunriseTime" className="text-md fill-gray-c3">
+          {format(sunrise, "hh:mm a")}
+        </text>
+      </g>
+      <g id="sunset-g">
+        <text id="sunset" className="text-sm fill-gray-c2">
+          Sunset
+        </text>
+        <text id="sunsetTime" className="text-md fill-gray-c3">
+          {format(sunset, "hh:mm a")}
+        </text>
+      </g>
+      <text id="horizon" className="text-sm fill-gray-c2">
+        Horizon
+      </text>
       <path id="startToSunrise" className="fill-blue-900"></path>
       <path id="sunriseToSunset" className=" fill-blue-300"></path>
       <path
